fb6daee19a1b3787edf4b2530b61e789e88472a8
Antti Hatanmaa <antti.hatanmaa@aalto.fi>
2016-06-11 23:50:41 +0300

    Add curvature to BSpline

--- include/BSpline.h
+++ include/BSpline.h
@@ -4,26 +4,30 @@
 #include <vector>
 #include <Eigen/Dense>
 
-class BSpline {
+class BSpline
+{
 public:
 	BSpline();
 
 	void addControlPoint (const Eigen::Vector3f&);
 	void setBasis        (const Eigen::Matrix4f&);
 
-	Eigen::Vector3f position (float t) const;
-	Eigen::Vector3f tangent  (float t) const;
+	Eigen::Vector3f position  (float t) const;
+	Eigen::Vector3f tangent   (float t) const;
+	Eigen::Vector3f curvature (float t) const;
 private:
 	Eigen::Matrix4f              m_basis;
-	Eigen::Matrix4f              m_derivativeBasis;
+	Eigen::Matrix4f              m_tangentBasis;
+	Eigen::Matrix4f              m_curvatureBasis;
 	std::vector<Eigen::Vector3f> m_controlPoints;
 
-	void buildDerivativeBasis();
+	void buildDerivativeBases();
 
 	enum EvalType
 	{
 		POSITION,
-		TANGENT
+		TANGENT,
+		CURVATURE
 	};
 	Eigen::Vector3f evaluate(float t, EvalType) const;
 };

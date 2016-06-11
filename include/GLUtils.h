22cfcd643d0ac1b8a13ecfdb55a8fae2b06a0077
Antti Hatanmaa <antti.hatanmaa@aalto.fi>
2016-06-11 22:47:05 +0300

    Fetch old GLUtils

--- include/GLUtils.h
+++ include/GLUtils.h
@@ -10,9 +10,20 @@
 
 namespace GLUtils
 {
+Eigen::Matrix4f perspective(int width, int height,
+                            float fov, float near = 0.1f, float far = 15.0f);
 
-    GL::ShaderProgram supercool_shader(void);
+Eigen::Matrix4f look_at(const Eigen::Vector3f& eye,
+                        const Eigen::Vector3f& target = Eigen::Vector3f(0, 0, 0),
+                        const Eigen::Vector3f& up_hint = Eigen::Vector3f(0, 1, 0));
 
+Eigen::Matrix3f ray_look_at(const Eigen::Vector3f& eye,
+                            const Eigen::Vector3f& target,
+                            const Eigen::Vector3f& up_hint,
+                            int width, int height,
+                            float fov = PI / 2);
+
+GL::ShaderProgram supercool_shader(void);
 } // namespace GLUtils
 
 #endif // GLUTILS_H
